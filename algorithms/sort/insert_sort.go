ac91d136aee55606c198290382b3b11fba5e620a
mark <ma_fengtao@163.com>
2018-07-25 10:52:42 +0800

    update insert sort

--- algorithms/sort/insert_sort.go
+++ algorithms/sort/insert_sort.go
@@ -1,11 +1,16 @@
 package sort
 
 func InsertSort(data []int) []int {
-	var key, pos int
 	total := len(data)
+	if total == 0 {
+		return nil
+	}
+
+	var key, pos int
 	for i := 0; i < total; i++ {
 		key = data[i]
 		pos = i
+
 		for pos > 0 && data[pos-1] > key {
 			data[pos] = data[pos-1]
 			pos = pos - 1
